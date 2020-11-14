3e109ba9c377fe2d37fe51adc091fafc02d51e5d
kinattaporn <kinattaporn@gmail.com>
2020-11-14 15:50:41 -0800

    first commit

---
+++ src/test/scala/objsets/TweetSetSuite.scala
@@ -0,0 +1,95 @@
+package objsets
+
+import org.junit._
+import org.junit.Assert.assertEquals
+
+class TweetSetSuite {
+  trait TestSets {
+    val set1 = new Empty
+    val set2 = set1.incl(new Tweet("a", "a body", 20))
+    val set3 = set2.incl(new Tweet("b", "b body", 20))
+    val c = new Tweet("c", "c body", 7)
+    val d = new Tweet("d", "d body", 9)
+    val set4c = set3.incl(c)
+    val set4d = set3.incl(d)
+    val set5 = set4c.incl(d)
+  }
+
+  def asSet(tweets: TweetSet): Set[Tweet] = {
+    var res = Set[Tweet]()
+    tweets.foreach(res += _)
+    res
+  }
+
+  def size(set: TweetSet): Int = asSet(set).size
+
+  @Test def `filter: on empty set`: Unit =
+    new TestSets {
+      assertEquals(0, size(set1.filter(tw => tw.user == "a")))
+    }
+
+  @Test def `filter: a on set5`: Unit =
+    new TestSets {
+      set5.foreach(println)
+      println("----------")
+      set5.filter(tw => tw.user == "a").foreach(println)
+      assertEquals(1, size(set5.filter(tw => tw.user == "a")))
+    }
+
+  @Test def `filter: twenty on set5`: Unit =
+    new TestSets {
+      set5.foreach(println)
+      println("----------")
+      set5.filter(tw => tw.retweets == 20).foreach(println)
+      assertEquals(2, size(set5.filter(tw => tw.retweets == 20)))
+    }
+
+  @Test def `union: set4c and set4d`: Unit =
+    new TestSets {
+      set4c.foreach(println)
+      println("----------")
+      set4c.union(set4d).foreach(println)
+      assertEquals(4, size(set4c.union(set4d)))
+    }
+
+  @Test def `union: with empty set1`: Unit =
+    new TestSets {
+      set5.foreach(println)
+      println("----------")
+      set5.union(set1).foreach(println)
+      assertEquals(4, size(set5.union(set1)))
+    }
+
+  @Test def `union: with empty set2`: Unit =
+    new TestSets {
+      set1.foreach(println)
+      println("----------")
+      set1.union(set5).foreach(println)
+      assertEquals(4, size(set1.union(set5)))
+    }
+
+  @Test def `union: set4c and set2 (already contain)`: Unit =
+    new TestSets {
+      set4c.foreach(println)
+      println("----------")
+      set4c.union(set2).foreach(println)
+      assertEquals(3, size(set4c.union(set2)))
+    }
+
+  @Test def `mostRetweeted test`: Unit =
+    new TestSets {
+      set4c.foreach(println)
+      println("----------")
+      println(set4c.mostRetweeted)
+    }
+
+  @Test def `descending: set5`: Unit =
+    new TestSets {
+      val trends = set5.descendingByRetweet
+      trends.foreach(println)
+      assert(!trends.isEmpty)
+      assert(trends.head.user == "a" || trends.head.user == "b")
+    }
+
+  @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
+}
